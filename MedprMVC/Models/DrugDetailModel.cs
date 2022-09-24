0e64ba42477fa4ec2cb733ade7b921f3d76dfb6e
lkumbrella <L.Kumbrella@outlook.com>
2022-09-24 18:01:31 +0300

    First view for drug details

---
+++ MedprMVC/Models/DrugDetailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MedprMVC.Models
+{
+    public class DrugDetailModel
+    {
+        public string Name { get; set; }
+        public string PharmGroup { get; set; }
+        public int Price { get; set; }
+    }
+}
